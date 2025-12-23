d482998ec09ba9a82537a5e151f3ac9e3a92e741
Daniel Jin <mrpickles123115@gmail.com>
2025-12-23 06:45:15 -0500

    lexer impl

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,23 +1,67 @@
 use crate::tokens::*;
 
+#[derive(Debug, Clone)]
 pub struct TokenInfo {
-    position: u32,
+    pub position: u32,
 }
 
+#[derive(Debug, Clone)]
 pub struct LexedToken {
-    info: TokenInfo,
-    token: Token,
+    pub info: TokenInfo,
+    pub token: Token,
 }
 
+#[derive(Debug, Clone)]
 pub enum LexerErrorReason {
-    UnknownToken(String),
+    UnknownToken(char),
 }
 
+#[derive(Debug, Clone)]
 pub struct LexerError {
-    info: TokenInfo,
-    reason: LexerErrorReason,
+    pub info: TokenInfo,
+    pub reason: LexerErrorReason,
 }
 
 pub fn lex(stream: &str) -> Result<Vec<LexedToken>, LexerError> {
-    Ok(vec![])
+    let mut tokens = Vec::new();
+    let mut iter = stream.chars().peekable();
+    let mut idx = 0u32;
+
+    while let Some(ch) = iter.next() {
+        idx += 1;
+        if ch.is_whitespace() {
+            continue;
+        }
+
+        let t = match ch {
+            '+' => Token::PLUS,
+            '-' => Token::MINUS,
+            '*' => Token::ASTER,
+            '/' => Token::SLASH,
+            '(' => Token::LPAREN,
+            ')' => Token::RPAREN,
+            '0' => Token::DIGIT(Digit::ZERO),
+            '1' => Token::DIGIT(Digit::ONE),
+            '2' => Token::DIGIT(Digit::TWO),
+            '3' => Token::DIGIT(Digit::THREE),
+            '4' => Token::DIGIT(Digit::FOUR),
+            '5' => Token::DIGIT(Digit::FIVE),
+            '6' => Token::DIGIT(Digit::SIX),
+            '7' => Token::DIGIT(Digit::SEVEN),
+            '8' => Token::DIGIT(Digit::EIGHT),
+            '9' => Token::DIGIT(Digit::NINE),
+            _ => {
+                return Err(LexerError {
+                    info: TokenInfo { position: idx },
+                    reason: LexerErrorReason::UnknownToken(ch),
+                });
+            }
+        };
+        let lexed = LexedToken {
+            info: TokenInfo { position: idx },
+            token: t,
+        };
+        tokens.push(lexed);
+    }
+    Ok(tokens)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,7 @@ pub mod tokens;
 
 fn main() {
     println!("Hello, world!");
+
+    let rs = lexer::lex("1 - 8 + 5 * 32 / 4 - 3");
+    print!("{:?}", rs);
 }
