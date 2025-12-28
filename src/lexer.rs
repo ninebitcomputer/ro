a90e6acdec3e14641778f9bbcbb16beddc322e0f
Daniel Jin <mrpickles123115@gmail.com>
2025-12-28 12:26:07 -0500

    fully iterable lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -13,94 +13,76 @@ pub struct LexedToken {
     pub token: Token,
 }
 
-#[derive(Debug, Clone)]
-pub enum LexerErrorReason {
-    UnknownToken(char),
-}
-
-#[derive(Debug, Clone)]
-pub struct LexerError {
-    pub info: TokenInfo,
-    pub reason: LexerErrorReason,
-}
-
-pub struct Lexer {
-    stream: String,
-}
-
-pub struct LexerIterator<'a> {
+pub struct Lexer<'a> {
     chars: Peekable<Chars<'a>>,
 }
 
-impl<'a> Iterator for LexerIterator<'a> {
-    type Item = Result<LexedToken, LexerError>;
+impl<'a> Lexer<'a> {
+    pub fn new(chars: Peekable<Chars<'a>>) -> Self {
+        Self { chars }
+    }
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(cur) = self.chars.peek() {
-            match *cur {
-                _ => Some(Err(LexerError {
-                    info: TokenInfo { position: idx },
-                    reason: LexerErrorReason::UnknownToken(ch),
-                })),
+    fn consume_whitespace(&mut self) {
+        while let Some(ch) = self.chars.peek()
+            && ch.is_whitespace()
+        {
+            self.chars.next();
+        }
+    }
+
+    fn read_num(&mut self) -> Option<u32> {
+        let mut result: u32 = 0;
+        let mut parsed: usize = 0;
+        while let Some(&ch) = self.chars.peek() {
+            if let Some(d) = ch.to_digit(10) {
+                result *= 10;
+                result += d;
+                parsed += 1;
+                self.chars.next();
+            } else {
+                break;
+            }
+        }
+        if parsed == 0 { None } else { Some(result) }
+    }
+
+    fn next_token(&mut self) -> Option<LexedToken> {
+        self.consume_whitespace();
+        if let Some(ch) = self.chars.peek() {
+            let tk = match ch {
+                '+' => Token::PLUS,
+                '-' => Token::MINUS,
+                '*' => Token::ASTER,
+                '/' => Token::SLASH,
+                '(' => Token::LPAREN,
+                ')' => Token::RPAREN,
+                _ => {
+                    if ch.is_digit(10) {
+                        if let Some(n) = self.read_num() {
+                            Token::NUMBER(n)
+                        } else {
+                            Token::ILLEGAL
+                        }
+                    } else {
+                        Token::ILLEGAL
+                    }
+                }
             };
-            None
+            self.chars.next();
+            Some(LexedToken {
+                info: TokenInfo { position: 0 },
+                token: tk,
+            })
         } else {
             None
         }
     }
 }
 
-impl Lexer {
-    pub fn new(stream: String) -> Self {
-        Self { stream }
-    }
-    pub fn iter(&'_ self) -> LexerIterator<'_> {
-        LexerIterator {
-            chars: self.stream.chars().peekable(),
-        }
-    }
-}
+impl<'a> Iterator for Lexer<'a> {
+    type Item = LexedToken;
 
-pub fn lex(stream: &str) -> Result<Vec<LexedToken>, LexerError> {
-    let mut tokens = Vec::new();
-    let mut iter = stream.chars().peekable();
-    let mut idx = 0u32;
-
-    while let Some(ch) = iter.next() {
-        idx += 1;
-        if ch.is_whitespace() {
-            continue;
-        }
-
-        let t = match ch {
-            '+' => Token::PLUS,
-            '-' => Token::MINUS,
-            '*' => Token::ASTER,
-            '/' => Token::SLASH,
-            '(' => Token::LPAREN,
-            ')' => Token::RPAREN,
-            '0' => Token::DIGIT(Digit::ZERO),
-            '1' => Token::DIGIT(Digit::ONE),
-            '2' => Token::DIGIT(Digit::TWO),
-            '3' => Token::DIGIT(Digit::THREE),
-            '4' => Token::DIGIT(Digit::FOUR),
-            '5' => Token::DIGIT(Digit::FIVE),
-            '6' => Token::DIGIT(Digit::SIX),
-            '7' => Token::DIGIT(Digit::SEVEN),
-            '8' => Token::DIGIT(Digit::EIGHT),
-            '9' => Token::DIGIT(Digit::NINE),
-            _ => {
-                return Err(LexerError {
-                    info: TokenInfo { position: idx },
-                    reason: LexerErrorReason::UnknownToken(ch),
-                });
-            }
-        };
-        let lexed = LexedToken {
-            info: TokenInfo { position: idx },
-            token: t,
-        };
-        tokens.push(lexed);
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_token()
     }
-    Ok(tokens)
 }
