09d4ab2f92c2cd30c2182c06e074d9786496ed35
Daniel Jin <mrpickles123115@gmail.com>
2026-01-07 18:18:19 -0500

    fix lexer over consuming characters and add hex/binary/octal numbers

--- src/lexer.rs
+++ src/lexer.rs
@@ -30,44 +30,80 @@ impl<'a> Lexer<'a> {
         }
     }
 
-    fn read_num(&mut self) -> Option<u32> {
+    fn read_num(&mut self, radix: u32) -> Option<u32> {
         let mut result: u32 = 0;
         let mut parsed: usize = 0;
         while let Some(&ch) = self.chars.peek() {
-            if let Some(d) = ch.to_digit(10) {
-                result *= 10;
+            if let Some(d) = ch.to_digit(radix) {
+                result *= radix;
                 result += d;
                 parsed += 1;
                 self.chars.next();
             } else {
+                // illegal
+                if ch.is_alphabetic() {
+                    self.chars.next();
+                    return None;
+                }
                 break;
             }
         }
         if parsed == 0 { None } else { Some(result) }
     }
 
+    fn match_single(c: char) -> Option<Token> {
+        match c {
+            '+' => Some(Token::PLUS),
+            '-' => Some(Token::MINUS),
+            '*' => Some(Token::ASTER),
+            '/' => Some(Token::SLASH),
+            '(' => Some(Token::LPAREN),
+            ')' => Some(Token::RPAREN),
+            _ => None,
+        }
+    }
+
+    fn expect_number(&mut self, radix: u32) -> Token {
+        if let Some(n) = self.read_num(radix) {
+            Token::NUMBER(n)
+        } else {
+            Token::ILLEGAL
+        }
+    }
+
     fn next_token(&mut self) -> Option<LexedToken> {
         self.consume_whitespace();
-        if let Some(ch) = self.chars.next() {
-            let tk = match ch {
-                '+' => Token::PLUS,
-                '-' => Token::MINUS,
-                '*' => Token::ASTER,
-                '/' => Token::SLASH,
-                '(' => Token::LPAREN,
-                ')' => Token::RPAREN,
-                _ => {
-                    if ch.is_digit(10) {
-                        if let Some(n) = self.read_num() {
-                            Token::NUMBER(n)
+        if let Some(ch) = self.chars.peek().cloned() {
+            let tk = if let Some(t) = Lexer::match_single(ch) {
+                self.chars.next();
+                t
+            } else {
+                match ch {
+                    '0' => {
+                        self.chars.next();
+                        if let Some(pfx) = self.chars.peek().cloned() {
+                            if pfx.is_numeric() {
+                                Token::ILLEGAL
+                            } else if pfx.is_alphabetic() {
+                                self.chars.next();
+                                match pfx {
+                                    'x' => self.expect_number(16),
+                                    'o' => self.expect_number(8),
+                                    'b' => self.expect_number(1),
+                                    _ => Token::ILLEGAL,
+                                }
+                            } else {
+                                Token::NUMBER(0)
+                            }
                         } else {
-                            Token::ILLEGAL
+                            Token::NUMBER(0)
                         }
-                    } else {
-                        Token::ILLEGAL
                     }
+                    '1'..='9' => self.expect_number(10),
+                    _ => Token::ILLEGAL,
                 }
             };
+
             Some(LexedToken {
                 info: TokenInfo { position: 0 },
                 token: tk,
--- src/main.rs
+++ src/main.rs
@@ -9,36 +9,10 @@ pub mod parser;
 pub mod tokens;
 
 fn main() {
-    //let tokens = lexer::lex("1 - 8 + 5 * 32 / 4 - 3").unwrap();
-    //let tree = ast::T0::parse(tokens.as_slice());
-
-    let s = "1 - 8 + 5 * 32 / 4 - 3 * (2 + 8)";
+    let s = "1-8+5*32/ 0x20 - 3 * (2 + 8)";
     let mut lexer = lexer::Lexer::new(s.chars().peekable());
 
     while let Some(t) = lexer.next() {
         println!("{:?}", t);
     }
-
-    //println!("{:?}", tree);
-    /* loop {
-        let mut buffer = String::new();
-        let stdin = std::io::stdin();
-        print!("> ");
-        stdout().flush().unwrap();
-        stdin.read_line(&mut buffer).unwrap();
-
-        let lex_result = lexer::lex(buffer.as_str());
-        match lex_result {
-            Ok(tokens) => {
-                if let Some(tree) = ast::T0::parse(tokens.as_slice()) {
-                    println!("{}", tree.eval());
-                } else {
-                    println!("Syntax error");
-                }
-            }
-            Err(_e) => {
-                println!("Error processing tokens")
-            }
-        }
-    } */
 }
